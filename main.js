e71c35439c861fa190b9742afcc28b99308c8faa
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-07-12 23:18:15 +0500

    Fix eslint errors

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const form = document.getElementById("form");
-const titleInput = document.getElementById("title-input");
-const authorInput = document.getElementById("author-input");
-const booksList = document.getElementById("booksList");
+const form = document.getElementById('form');
+const titleInput = document.getElementById('title-input');
+const authorInput = document.getElementById('author-input');
+const booksList = document.getElementById('booksList');
 
 let books = [];
 let id = 0;
 
 function addBook() {
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 }
 class Collection {
   static loadBooks() {
-    const storedBooks = JSON.parse(localStorage.getItem("books"));
+    const storedBooks = JSON.parse(localStorage.getItem('books'));
     if (storedBooks) {
       books = storedBooks;
       books.forEach((book) => Collection.createBook(book));
@@ -19,7 +19,7 @@ class Collection {
   }
 
   static createBook(book) {
-    const addNewBook = document.createElement("tr");
+    const addNewBook = document.createElement('tr');
     addNewBook.innerHTML = `
       <td>"${book.title}" by ${book.author}
       <button type="button" class="remove-btn" data-id="${book.id}">Remove</button>
@@ -29,27 +29,27 @@ class Collection {
   }
 
   static clearingFields() {
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   }
 
   static removeBook(id) {
     const index = books.findIndex((book) => book.id === id);
     if (index !== -1) {
       books.splice(index, 1);
-      booksList.innerHTML = "";
+      booksList.innerHTML = '';
       books.forEach((book) => Collection.createBook(book));
       addBook();
     }
   }
 }
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   const title = titleInput.value;
   const author = authorInput.value;
 
-  if (title === "" || author === "") {
-    alert("Please Add title and author");
+  if (title === '' || author === '') {
+    alert('Please Add title and author');
     return;
   }
 
@@ -64,11 +64,11 @@ form.addEventListener("submit", (event) => {
   addBook();
 });
 
-document.addEventListener("DOMContentLoaded", Collection.loadBooks);
+document.addEventListener('DOMContentLoaded', Collection.loadBooks);
 
-booksList.addEventListener("click", (event) => {
-  if (event.target.classList.contains("remove-btn")) {
-    const id = parseInt(event.target.getAttribute("data-id"), 10);
+booksList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-btn')) {
+    const id = parseInt(event.target.getAttribute('data-id'), 10);
     Collection.removeBook(id);
   }
 });
