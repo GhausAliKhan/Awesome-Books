c308ed0838be57947dfedf1f6514dcf1e3c3d796
codeStackr <rdzaman20000@gmail.com>
2023-07-13 00:13:22 +0600

    Create class method to add and remove books

--- main.js
+++ main.js
@@ -9,7 +9,40 @@ let id = 0;
 function addBook() {
   localStorage.setItem("books", JSON.stringify(books));
 }
+class Collection {
+  static loadBooks() {
+    const storedBooks = JSON.parse(localStorage.getItem("books"));
+    if (storedBooks) {
+      books = storedBooks;
+      books.forEach((book) => Collection.createBook(book));
+    }
+  }
 
+  static createBook(book) {
+    const addNewBook = document.createElement("tr");
+    addNewBook.innerHTML = `
+      <td>"${book.title}" by ${book.author}
+      <button type="button" class="remove-btn" data-id="${book.id}">Remove</button>
+      </td>
+    `;
+    booksList.appendChild(addNewBook);
+  }
+
+  static clearingFields() {
+    titleInput.value = "";
+    authorInput.value = "";
+  }
+
+  static removeBook(id) {
+    const index = books.findIndex((book) => book.id === id);
+    if (index !== -1) {
+      books.splice(index, 1);
+      booksList.innerHTML = "";
+      books.forEach((book) => Collection.createBook(book));
+      addBook();
+    }
+  }
+}
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const title = titleInput.value;
@@ -26,16 +59,16 @@ form.addEventListener("submit", (event) => {
     id: (id += 1),
   };
   books.push(book);
-  createBook(book);
-  clearingFields();
+  Collection.createBook(book);
+  Collection.clearingFields();
   addBook();
 });
 
-document.addEventListener("DOMContentLoaded", loadBooks());
+document.addEventListener("DOMContentLoaded", Collection.loadBooks);
 
 booksList.addEventListener("click", (event) => {
   if (event.target.classList.contains("remove-btn")) {
     const id = parseInt(event.target.getAttribute("data-id"), 10);
-    removeBook(id);
+    Collection.removeBook(id);
   }
 });
