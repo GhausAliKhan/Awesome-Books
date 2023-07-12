0a3bfba900d82a84ef5c1694ffc0dce661c6c8a5
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-07-12 22:36:53 +0500

    Updated Html and Add style.css

--- main.js
+++ main.js
@@ -1,81 +1,22 @@
-const form = document.getElementById('form');
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
-const booksList = document.getElementById('booksList');
+const form = document.getElementById("form");
+const titleInput = document.getElementById("title-input");
+const authorInput = document.getElementById("author-input");
+const booksList = document.getElementById("booksList");
 
 let books = [];
 let id = 0;
 
-//  Inner Html Generating Books
-
-function createBook(book) {
-  const newBook = document.createElement('li');
-  newBook.innerHTML = `
-    <div>${book.title}</div>
-    <div>${book.author}</div>
-    <button type="button" class="remove-btn" data-id="${book.id}">Remove</button>
-    <hr>
-  `;
-  booksList.appendChild(newBook);
-}
-// Clearing Fields
-
-function clearingFields() {
-  titleInput.value = '';
-  authorInput.value = '';
-}
-
-// Remove Book
-
-function removeBook(id) {
-  const index = books.findIndex((book) => book.id === id);
-  if (index !== -1) {
-    books.splice(index, 1);
-  }
-}
-
-// Update Books
-
-function updateBooks() {
-  while (booksList.firstChild) {
-    booksList.firstChild.remove();
-  }
-  books.forEach((book) => createBook(book));
-}
-
-// Store books
-
 function addBook() {
-  localStorage.setItem('Books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
 }
 
-// Load books
-
-function loadCollection() {
-  const storedBooks = JSON.parse(localStorage.getItem('Books'));
-  if (storedBooks) {
-    books = storedBooks;
-    updateBooks();
-  }
-}
-
-booksList.addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove-btn')) {
-    const id = parseInt(event.target.getAttribute('data-id'), 10);
-    removeBook(id);
-    updateBooks();
-    addBook();
-  }
-});
-
-// Creating collection
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   const title = titleInput.value;
   const author = authorInput.value;
 
-  if (title === '' || author === '') {
-    alert('Please Add title and author');
+  if (title === "" || author === "") {
+    alert("Please Add title and author");
     return;
   }
 
@@ -84,11 +25,17 @@ form.addEventListener('submit', (event) => {
     author,
     id: (id += 1),
   };
-
   books.push(book);
   createBook(book);
   clearingFields();
   addBook();
 });
 
-loadCollection();
+document.addEventListener("DOMContentLoaded", loadBooks());
+
+booksList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-btn")) {
+    const id = parseInt(event.target.getAttribute("data-id"), 10);
+    removeBook(id);
+  }
+});
